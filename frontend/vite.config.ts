5eb366f13295b0685c3c79b6fa12c828566a2979
Ethan <ethanling123@gmail.com>
2024-12-11 09:58:22 -0500

    process fix hope

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import { API_BASE_URL } from "./src/config/api";
 
 export default defineConfig({
+    define: {
+        "process.env": process.env,
+    },
     plugins: [react()],
     server: {
         proxy: {
