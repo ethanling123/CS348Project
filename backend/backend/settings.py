b58e68703d99d22afee2aa190c2c374e171a7adb
Ethan <ethanling123@gmail.com>
2024-12-11 05:54:42 -0500

    - Added django-environ to Pipfile for improved environment variable management. - Updated Pipfile.lock to include django-environ with version 0.11.2 and updated the hash. - Refactored settings.py to utilize django-environ for loading environment variables, enhancing security and configuration management. - Expanded ALLOWED_HOSTS in settings.py to include 'localhost' for local development.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,22 +13,24 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-import django_heroku
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inventory-manage-f1a169cae41e.herokuapp.com']
+ALLOWED_HOSTS = ['inventory-manage-f1a169cae41e.herokuapp.com', 'localhost']
 
 
 # Application definition
